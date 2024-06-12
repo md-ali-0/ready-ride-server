1a18904adf383c333674e6bd89f59dfed133f33d
md-ali-0 <mohammad.98482@gmail.com>
2024-06-12 23:46:55 +0600

    User Serivce Updated

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,12 +1,15 @@
 import { IUser } from './user.interface'
 import { User } from './user.model'
 
-const getProfile = async (email: string) => {
+const getProfile = async (email: string): Promise<IUser | null> => {
     const user = await User.findOne({ email })
     return user
 }
 
-const updateProfile = async (email: string, payload: Partial<IUser>) => {
+const updateProfile = async (
+    email: string,
+    payload: Partial<IUser>,
+): Promise<IUser | null> => {
     const user = await User.findOneAndUpdate({ email }, payload, { new: true })
     return user
 }
